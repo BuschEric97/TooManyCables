7148b1a37287bca43d4ded727fcc71aa47b0229e
Eric Busch <buscheric97@gmail.com>
2025-01-24 17:24:23 -0600

    Corrected profile page tsx filename

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
+import "./../../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
@@ -15,30 +15,6 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
-  const { signOut } = useAuthenticator();
-
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createGame() {
-    client.models.Todo.create({
-      content: window.prompt("Game"),
-    });
-  }
-
-  function deleteGame(id: string) {
-    client.models.Todo.delete({id})
-  }
-
   return (
     <main>
       <h1>Too Many Cables</h1>
